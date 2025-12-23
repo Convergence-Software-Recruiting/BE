31045e835bfc3b44e04044a8b77a983cc0851143
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-12-23 16:52:28 +0900

    초기 세팅

---
+++ src/main/java/com/example/convergencesoftwarerecruitingbe/ConvergenceSoftwareRecruitingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.convergencesoftwarerecruitingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvergenceSoftwareRecruitingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvergenceSoftwareRecruitingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/convergencesoftwarerecruitingbe/ConvergenceSoftwareRecruitingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convergencesoftwarerecruitingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvergenceSoftwareRecruitingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
