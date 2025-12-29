4408a05b5bed9910d43ed95029bd2d5c8a9dfcd5
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-12-29 17:00:57 +0900

    fix: force Swagger API server to use https

---
+++ src/main/java/com/example/convergencesoftwarerecruitingbe/global/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.convergencesoftwarerecruitingbe.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl("https://recruit.bluerack.org");
+        return new OpenAPI().servers(List.of(server));
+    }
+}
